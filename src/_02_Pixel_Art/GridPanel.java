c071eb77c6b2c7001565e177b584694fb595643b
Rishi Khurana <league@WTS14.attlocal.net>
2018-09-30 11:26:26 -0700

    finished pixel art

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -3,10 +3,11 @@ package _02_Pixel_Art;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
-public class GridPanel extends JPanel{
+public class GridPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private int windowWidth;
@@ -15,62 +16,68 @@ public class GridPanel extends JPanel{
 	private int pixelHeight;
 	private int rows;
 	private int cols;
-	
-	//1. Create a 2D array of pixels. Do not initialize it yet.
-	Pixel [][] pixels;
+
+	// 1. Create a 2D array of pixels. Do not initialize it yet.
+	Pixel[][] pixels;
 	private Color color;
-	
+
 	public GridPanel(int w, int h, int r, int c) {
 		this.windowWidth = w;
 		this.windowHeight = h;
 		this.rows = r;
 		this.cols = c;
-		
+
 		this.pixelWidth = windowWidth / cols;
 		this.pixelHeight = windowHeight / rows;
-		
+
 		color = Color.BLACK;
-		
+
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
-		
-		//2. Initialize the pixel array using the rows and cols variables.
-		pixels = new Pixel [rows][cols];
-		
-		//3. Iterate through the array and initialize each element to a new pixel.
+
+		// 2. Initialize the pixel array using the rows and cols variables.
+		pixels = new Pixel[rows][cols];
+
+		// 3. Iterate through the array and initialize each element to a new pixel.
 		for (int i = 0; i < pixels.length; i++) {
-			for (int j = 0; j < pixels[i].length; i++) {
+			for (int j = 0; j < pixels[i].length; j++) {
 				pixels[i][j] = new Pixel(rows, cols);
 			}
 		}
-		
+
 	}
-	
+
 	public void setColor(Color c) {
 		color = c;
 	}
-	
+
 	public void clickPixel(int mouseX, int mouseY) {
-		//5. Use the mouseX and mouseY variables to change the color
-		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
-		for (int i = 0; i < pixels.length; i++) {
-			for (int j = 0; j < pixels[i].length; j++) {
-				Color c = new Color(mouseX, mouseY, (i*j)%256);
-				
-			}
-		}
+		// 5. Use the mouseX and mouseY variables to change the color
+		// of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		Random rRandom = new Random();
+		Random gRandom = new Random();
+		Random bRandom = new Random();
+		int x = mouseX / pixelWidth;
+		int y = mouseY / pixelHeight;
+		// int r = rRandom.nextInt(250);
+		// int g = gRandom.nextInt(250);
+		// int b = bRandom.nextInt(250);
+
+		pixels[x][y].color = color;
+		// setColor(pixels[x][y].color);
+		System.out.println("hi");
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
-		//4. Iterate through the array.
-		//   For every pixel in the list, fill in a rectangle using the pixel's color.
-		//   Then, use drawRect to add a grid pattern to your display.
+		// 4. Iterate through the array.
+		// For every pixel in the list, fill in a rectangle using the pixel's color.
+		// Then, use drawRect to add a grid pattern to your display.
 		for (int i = 0; i < pixels.length; i++) {
 			for (int j = 0; j < pixels[i].length; j++) {
-				g.setColor(Color.RED);
-				g.drawRect(i,j,WIDTH, HEIGHT);
+				g.setColor(pixels[i][j].color);
+				g.drawRect(i * pixelWidth, j * pixelHeight, pixelWidth, pixelHeight);
 			}
 		}
-		
+
 	}
 }
